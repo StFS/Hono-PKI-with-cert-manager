09a0fd324f2aa1961753bc8fcbe3d4a434aa40ad
Stefán Freyr Stefánsson <stefan.freyr@controlant.com>
2023-07-23 00:05:04 +0000

    Archiving project

--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-helm uninstall -n hono eclipse-hono --wait
-helm uninstall -n cert-manager reflector --wait
-helm uninstall -n cert-manager trust-manager --wait
-helm uninstall -n cert-manager cert-manager --wait
-
-kubectl delete namespace cert-manager
-kubectl delete namespace hono
